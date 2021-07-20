41346d33eba03fd523232108ec33f9426d419515
Chat801SHG <87728017+Chat801SHG@users.noreply.github.com>
2021-07-20 16:35:21 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,11 @@
-
+var firebaseConfig = {
+    apiKey: "AIzaSyDjBcmqwe-KQ4MsvIGSpEdPyvwf_TrIp90",
+    authDomain: "chat801shg-82732.firebaseapp.com",
+    projectId: "chat801shg-82732",
+    storageBucket: "chat801shg-82732.appspot.com",
+    messagingSenderId: "415763429738",
+    appId: "1:415763429738:web:7819d142665e0d95964d13",
+    measurementId: "G-VC6KH08GZ1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
